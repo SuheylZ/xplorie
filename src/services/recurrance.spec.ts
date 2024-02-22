6447965e07e6bc92f3f60a3ec4c3b1a5f82ff1a7
Einfach.Kerl <suheylz@hotmail.com>
2024-02-22 20:31:28 +0500

    dates fixed

--- src/services/recurrance.spec.ts
+++ src/services/recurrance.spec.ts
@@ -1,62 +1,112 @@
-import { Days, daily, weeklyOnce } from "./recurrance"
+import dayjs from "dayjs"
+import { Days, daily, _weeklyByDay, weekly, Months } from "./recurrance"
+
+function unwind(map: Map<{ month: number; year: number }, dayjs.Dayjs[]>) {
+  const allDays: dayjs.Dayjs[] = []
+  const allMonths: number[] = []
+
+  for (const [key, value] of map) {
+    allDays.push(...value)
+    allMonths.push(key.month)
+  }
+  return [allDays, allMonths] as [dayjs.Dayjs[], number[]]
+}
 
 describe("recurrance", () => {
-  afterAll(() => {})
+  const thursdayFeb1 = new Date(2024, 1, 1) //Thursday, Feb 1, 2024
+  const fridayMarch1 = new Date(2024, 2, 1) // Friday, March 1, 2024
 
-  const startsOn = new Date(2024, 1, 1) //Thursday
+  describe("internal functions", () => {
+    describe("Weekly single day, limit: number", () => {
+      const limit = 5
+
+      test("same day", () => {
+        const a1 = _weeklyByDay(thursdayFeb1, Days.Thursday, limit)
+        expect(a1.length).toBe(5)
+        a1.forEach((x) => expect(x.day()).toBe(Days.Thursday))
+      })
+
+      test("past day", () => {
+        const a1 = _weeklyByDay(thursdayFeb1, Days.Monday, limit)
+        expect(a1.length).toBe(5)
+        a1.forEach((x) => expect(x.day()).toBe(Days.Monday))
+      })
+
+      test("future day", () => {
+        const a1 = _weeklyByDay(thursdayFeb1, Days.Friday, limit)
+        expect(a1.length).toBe(5)
+        a1.forEach((x) => expect(x.day()).toBe(Days.Friday))
+      })
+    })
+
+    describe("Weekly single day, limit: Date", () => {
+      test("same day", () => {
+        const a1 = _weeklyByDay(thursdayFeb1, Days.Saturday, fridayMarch1)
+        expect(a1.length).toBe(4)
+        a1.forEach((x) => expect(x.day()).toBe(Days.Saturday))
+      })
+
+      test("past day", () => {
+        const a1 = _weeklyByDay(thursdayFeb1, Days.Monday, fridayMarch1)
+        expect(a1.length).toBe(4)
+        a1.forEach((x) => expect(x.day()).toBe(Days.Monday))
+      })
+
+      test("future day", () => {
+        const a1 = _weeklyByDay(thursdayFeb1, Days.Friday, fridayMarch1)
+        expect(a1.length).toBe(4)
+        a1.forEach((x) => expect(x.day()).toBe(Days.Friday))
+      })
+    })
+  })
 
   describe("daily", () => {
     test("daily", () => {
-      const a1 = daily(startsOn, 10)
+      const a1 = daily(thursdayFeb1, 10)
       expect(a1.size).toBe(1)
     })
 
     test("daily", () => {
-      const a1 = daily(startsOn, new Date(2024, 2, 1))
+      const a1 = daily(thursdayFeb1, new Date(2024, 2, 1))
       expect(a1.size).toBe(2)
     })
   })
 
-  describe("Weekly by Count", () => {
-    const limit = 5
+  describe("weekly", () => {
+    test("limit by date", () => {
+      const a1 = weekly(
+        thursdayFeb1,
+        [Days.Saturday, Days.Thursday],
+        fridayMarch1
+      )
 
-    test("weekly (day same)", () => {
-      const a1 = weeklyOnce(startsOn, Days.Thursday, limit)
-      expect(a1.length).toBe(5)
-      a1.forEach((x) => expect(x.day()).toBe(Days.Thursday))
-    })
-    test("weekly (day past)", () => {
-      const a1 = weeklyOnce(startsOn, Days.Monday, limit)
-      expect(a1.length).toBe(5)
-      a1.forEach((x) => expect(x.day()).toBe(Days.Monday))
-    })
+      const [allDays, allMonths] = unwind(a1)
 
-    test("weekly (day ahead)", () => {
-      const a1 = weeklyOnce(startsOn, Days.Friday, limit)
-      expect(a1.length).toBe(5)
-      a1.forEach((x) => expect(x.day()).toBe(Days.Friday))
-    })
-  })
-
-  describe("weekly by date", () => {
-    const endsOn = new Date(2024, 2, 1)
-
-    test("weekly (day same) by date", () => {
-      const a1 = weeklyOnce(startsOn, Days.Saturday, endsOn)
-      expect(a1.length).toBe(5)
-      a1.forEach((x) => expect(x.day()).toBe(Days.Saturday))
+      allDays.forEach((x) =>
+        expect(
+          x.day() === Days.Saturday || x.day() === Days.Thursday
+        ).toBeTruthy()
+      )
+      expect(allMonths.length).toBe(2)
+      allMonths.forEach((x) =>
+        expect(x === Months.February || x === Months.March).toBeTruthy()
+      )
     })
 
-    test("weekly (day past) by date", () => {
-      const a1 = weeklyOnce(startsOn, Days.Monday, endsOn)
-      expect(a1.length).toBe(5)
-      a1.forEach((x) => expect(x.day()).toBe(Days.Monday))
-    })
+    test("limit by Count", () => {
+      const a1 = weekly(thursdayFeb1, [Days.Saturday, Days.Thursday], 4)
 
-    test("weekly (day ahead) by date", () => {
-      const a1 = weeklyOnce(startsOn, Days.Friday, endsOn)
-      expect(a1.length).toBe(5)
-      a1.forEach((x) => expect(x.day()).toBe(Days.Friday))
+      const [allDays, allMonths] = unwind(a1)
+
+      expect(allDays.length).toBe(4)
+      expect(allMonths.length).toBe(1)
+      expect(allMonths.at(0)).toBe(Months.February)
+
+      allDays.forEach((x) =>
+        expect(
+          x.day() === Days.Saturday || x.day() === Days.Thursday
+        ).toBeTruthy()
+      )
     })
   })
 })
--- src/services/recurrance.ts
+++ src/services/recurrance.ts
@@ -14,7 +14,7 @@ export enum Days {
 }
 
 export enum Months {
-  January = 1,
+  January = 0,
   February,
   March,
   May,
@@ -56,11 +56,10 @@ dayjs.updateLocale("en", {
   ]
 })
 
-export type Schedule = {
-  Month: Months
-  Year: number
-  Days: Array<number>
-}
+/**
+ * Map<{ month: number; year: number }, dayjs.Dayjs[]>
+ */
+export type Schedule = Map<{ month: number; year: number }, dayjs.Dayjs[]>
 
 export type Circuit = Array<Schedule>
 export const EmptyCircuit: Circuit = []
@@ -71,16 +70,20 @@ const date = (a: Date) => dayjs(a).add(1, "day")
 const isDateLimit = (r: RecurranceLimit): r is Date => r instanceof Date
 const isCountLimit = (r: RecurranceLimit): r is number => typeof r === "number"
 
-export function weeklyOnce(start: Date, day: Days, limit: RecurranceLimit) {
+export function _weeklyByDay(start: Date, day: Days, limit: RecurranceLimit) {
   const startsOn = date(start)
   const arr: dayjs.Dayjs[] = []
 
   const current = (() => {
-    const diff = day - startsOn.day()
+    const diff = startsOn.day() - day
 
-    if (diff < 0) return startsOn.add(Math.abs(diff) + 1, "day")
-    else if (diff > 0) return startsOn.add(diff, "day").add(7, "day")
-    else return startsOn
+    if (diff < 0) {
+      return startsOn.add(Math.abs(diff), "day")
+    } else if (diff > 0) {
+      return startsOn.add(7 - diff, "day")
+    } else {
+      return startsOn.add(0, "day")
+    }
   })()
 
   const process = (c: dayjs.Dayjs) => {
@@ -94,18 +97,26 @@ export function weeklyOnce(start: Date, day: Days, limit: RecurranceLimit) {
       latest = process(latest)
     }
   } else {
-    while (current.isBefore(limit) || current.isSame(limit)) {
+    const endsOn = dayjs(limit)
+    while (latest.isBefore(endsOn) || latest.isSame(endsOn)) {
       latest = process(latest)
     }
   }
   return arr
 }
-function groupDates(arg: dayjs.Dayjs[]) {
-  return groupBy(arg, (x) => {
+function toSchedule(arg: dayjs.Dayjs[]) {
+  const schedule: Schedule = groupBy(arg, (x) => {
     return { month: x.month(), year: x.year() }
   })
-}
 
+  return schedule
+}
+/**
+ * calculates daily recurrance either by date or count
+ * @param start Date, where to start from
+ * @param limit Date|number, how many dates to calculate
+ * @returns Schedule
+ */
 export function daily(start: Date, limit: RecurranceLimit) {
   const startsOn = date(start)
   const arr: dayjs.Dayjs[] = []
@@ -120,18 +131,41 @@ export function daily(start: Date, limit: RecurranceLimit) {
     arr.push(latest.add(i, "day"))
   }
 
-  return groupDates(arr)
+  return toSchedule(arr)
 }
+/**
+ * calculates weekly recurrance by days limited either by date or count
+ * @param start Date, where to start from
+ * @param days Days[], days for which to calculate
+ * @param limit Date|number, how many dates to be generated
+ * @returns Schedule
+ */
 export function weekly(start: Date, days: Days[], limit: RecurranceLimit) {
-  const a1 = unique(days).flatMap((day) => weeklyOnce(start, day, limit))
-  const a2 = unique(a1)
-  const a3 = groupBy(a2, (x) => {
-    return { month: x.month(), year: x.year() }
-  })
-  return a3
+  const set = new Set<dayjs.Dayjs>()
+  const calculate = (day: Days) => _weeklyByDay(start, day, limit)
+
+  for (const day of unique(days)) {
+    const res = calculate(day)
+    res.forEach((x) => set.add(x))
+  }
+
+  const arr = isDateLimit(limit)
+    ? Array.from(set)
+    : Array.from(set).splice(0, limit)
+
+  return toSchedule(arr)
 }
+/**
+ * Claculates monthly recurrance either by day order or by day
+ * @param start Date, start date
+ * @param type "ByWeekday"|"ByDate"
+ * @param limit Date|number, to limit the number of dates
+ * @returns Schedule
+ */
 // export function monthly(
 //   start: Date,
 //   type: "byWeekday" | "byDate",
 //   limit: RecurranceLimit
-// ) {}
+// ) {
+//   return {} as Schedule
+// }
